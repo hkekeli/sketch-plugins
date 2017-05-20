99c2a6f2adbc18bbaad420166a3f1dc966a9a50a
Kekeli Sackey <hkekeli@users.noreply.github.com>
2017-05-20 16:24:13 -0400

    update plugins

--- Sketch Aspect Ratio/src/commands/index.js
+++
@@ -1,5 +0,0 @@
-import setAspectRatioManual from './setAspectRatioManual'
-import setAspectRatioList from './setAspectRatioList'
-import setAspectRatioRepeat from './setAspectRatioRepeat'
-
-export { setAspectRatioManual, setAspectRatioList, setAspectRatioRepeat }
--- Sketch Aspect Ratio/src/utils/sketch-deprecated.js
+++
@@ -1,3 +0,0 @@
-export function hasMSArray () {
-  return (typeof MSArray !== 'undefined')
-}
